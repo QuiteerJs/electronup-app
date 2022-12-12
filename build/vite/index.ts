aca485c7c54d4f9f0fd3c7a7915dd5ea4c580e53
quiteer <p602985@yeah.net>
2022-12-12 23:38:39 +0800

    vite配置

--- build/vite/index.ts
+++ build/vite/index.ts
@@ -1,8 +1,10 @@
 import { resolve } from 'path'
 import type { ViteConfig } from '@quiteer/electronup'
+import VueMacros from './plugins/macros'
 
 export default (root: string): ViteConfig => {
   return {
-    resolve: { alias: { '@': resolve(root, 'render') } }
+    resolve: { alias: { '@': resolve(root, 'render') } },
+    plugins: [VueMacros]
   }
 }
---
+++ build/vite/plugins/macros.ts
@@ -0,0 +1,10 @@
+import VueMacros from 'unplugin-vue-macros/vite'
+import Vue from '@vitejs/plugin-vue'
+import VueJsx from '@vitejs/plugin-vue-jsx'
+
+export default VueMacros({
+  plugins: {
+    vue: Vue(),
+    vueJsx: VueJsx()
+  }
+})
