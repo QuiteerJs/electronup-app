7941ca8085345fc94b1cad35eb72d2ca15131770
ZAM <p602985@yeah.net>
2022-12-20 14:56:12 +0800

    完善ts配置

--- render/main.ts
+++ render/main.ts
@@ -1,16 +1,19 @@
 import { createApp } from 'vue'
 import './styles/style.css'
 import { EventKeys, IpcWindowOptions } from '@quiteer/electron-ipc/web'
+import naive from 'naive-ui'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(createPinia()).use(naive).mount('#app')
 
 console.log(import.meta.env)
 
 // 窗口最大化
 window.$ipc.send(EventKeys.WindowOptionsKey, IpcWindowOptions.MAXIMIZE)
-
 setTimeout(() => {
   // 取消最大化
   window.$ipc.send(EventKeys.WindowOptionsKey, IpcWindowOptions.UNMAXIMIZE)
 }, 1000)
+
