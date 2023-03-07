fcc2c8b7d16e357aa98247dda7eed4a4d6125cea
ZAM <p602985@yeah.net>
2023-03-07 17:41:06 +0800

    使用youliso包创建窗口

--- main/index.ts
+++ main/index.ts
@@ -1,45 +1,64 @@
 import { BrowserWindow } from 'electron'
 import { Ipc } from '@quiteer/electron-ipc'
 import preload from '@quiteer/electron-preload'
-import { appInstance } from '@youliso/electronic/app'
+import { appInstance, logError, windowInstance } from '@youliso/electronic'
 import { loadUrl } from './absolutePath'
 
-appInstance.start().then(() => {
+appInstance.start().then(async () => {
   Ipc.init()
 
-  const win = new BrowserWindow({
-    height: 700,
-    width: 1000,
-    webPreferences: {
-      sandbox: false,
-      preload: preload as string
-    }
+  windowInstance.setDefaultCfg({
+    defaultUrl: loadUrl,
+    defaultPreload: preload as string
   })
 
-  const child = new BrowserWindow({
-    parent: win,
-    height: 700,
-    width: 200,
-    show: false,
-    webPreferences: {
-      sandbox: false,
-      preload: preload as string
+  const mainId = await windowInstance.create(
+    {
+      title: 'main',
+      route: '/',
+      headNative: true
+    },
+    {
+      height: 700,
+      width: 1000,
+      webPreferences: {
+        sandbox: false
+      }
     }
-  })
+  )
 
-  win.loadURL(loadUrl)
+  const win = BrowserWindow.fromId(mainId!)!
 
-  win.webContents.openDevTools({ mode: 'right' })
+  const childId = await windowInstance.create(
+    {
+      title: 'child',
+      route: '/',
+      headNative: true,
+      parentId: mainId
+    },
+    {
+      height: 700,
+      width: 400,
+      show: false,
+      webPreferences: {
+        sandbox: false
+      }
+    }
+  )
+  const child = BrowserWindow.fromId(childId!)!
+
   win.once('ready-to-show', () => {
     setTimeout(() => {
       const [x, y] = win.getPosition()
+      console.log('x, y: ', x, y)
       child.setPosition(x + 990, y)
       child.show()
-    }, 1000)
+    }, 1300)
   })
 
   win.on('will-move', (event, newBounds) => {
     child.setPosition(newBounds.x + 990, newBounds.y)
   })
 })
+  .catch(logError)
 
